0b2d64cf03ae49725b95ad68bd712a40b718f419
Thai Le <thai.le@vice.com>
2014-02-26 18:40:07 -0500

    added basic styles, separated data fixtures into its own folder

--- app/fixtures/projects.js
+++ app/fixtures/projects.js
@@ -3,10 +3,12 @@ define([], function() {
 		id: 1,
 		title: 'VICE News',
 		url: 'http://news.vice.com',
+		image: '',
 	}, {
 		id: 2,
 		title: 'Brisk Bodega',
-		url: 'http://briskbodega.com'
+		url: 'http://briskbodega.com',
+		image: '',
 	}];
 
 	return projects;
